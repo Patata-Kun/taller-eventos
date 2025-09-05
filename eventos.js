a354ceca41f7c837c36bd04aefa3f8de82a262b0
Patata-Kun <jeremias.cardona.b@gmail.com>
2025-09-05 10:25:01 -0300

    v1.1.0

--- eventos.js
+++ eventos.js
@@ -4,3 +4,10 @@ div.addEventListener("click", () => {
   event.stopPropagation()
   alert("¡Hola! Soy el div");
 });
+
+const button = document.querySelector(".divButton button");
+
+button.addEventListener("click", (event) => {
+  event.stopPropagation();
+  alert("¡Hola! Soy el botón");
+});
